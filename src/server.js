2b8b8ad45510e4f0b49b5284453ff9a2e1589b9c
Edesio Costa <edesiojnr@gmail.com>
2020-09-04 23:23:57 -0300

    Atualizando Site Proffys

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const proffys = [
     { 
         name: "Diego Fernandes",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "86998265190",
+        whatsapp:"86998265190",
         bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject: "Química",
         cost:"20",
@@ -13,14 +13,28 @@ const proffys = [
     { 
         name: "Daniele Evangelista",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "86998265190",
+        whatsapp:"86998265190",
         bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject: "Química",
         cost:"20",
         weekday:[1],
         time_from:[720],
         time_to:[1220]
+    },
+    { 
+        name: "Maik Brito",
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsapp:"86998265190",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost:"20",
+        weekday: [1],
+        time_from:[720],
+        time_to:[1220]
     }
+
+
+    
 ]
 
 function pageLanding(req, res) {
@@ -28,7 +42,7 @@ function pageLanding(req, res) {
 }
 
 function pageStudy(req, res) {
-    return res.render("study.html")
+    return res.render("study.html", {  proffys })
 }
 
 function pageGiveclasses(req, res) {
@@ -55,5 +69,5 @@ server
 .get("/give-classes", pageGiveclasses)
 .listen(5500)
 
-
+ 
 
