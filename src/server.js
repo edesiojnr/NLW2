0a071d5f05ef2b4987f1a523987dd5890326c987
Edesio Costa <edesiojnr@gmail.com>
2020-09-08 21:42:58 -0300

    NLW Finalizado

--- src/server.js
+++ src/server.js
@@ -1,73 +1,34 @@
-const proffys = [
-    { 
-        name: "Diego Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp:"86998265190",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost:"20",
-        weekday:[0],
-        time_from:[720],
-        time_to:[1220]
-    },
-    { 
-        name: "Daniele Evangelista",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp:"86998265190",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost:"20",
-        weekday:[1],
-        time_from:[720],
-        time_to:[1220]
-    },
-    { 
-        name: "Maik Brito",
-        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
-        whatsapp:"86998265190",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost:"20",
-        weekday: [1],
-        time_from:[720],
-        time_to:[1220]
-    }
-
-
-    
-]
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    return res.render("study.html", {  proffys })
-}
-
-function pageGiveclasses(req, res) {
-    return res.render("give-classes.html")
-}
-
+// Servidor
 const express = require('express')
 const server = express()
 
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
 
-// configurar nunjucks
+
+// configurar nunjucks (template engine)
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
     express: server,
     noCache: true,
 })
 
+//Inicio e configuração do servidor
 server
+//receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
 // configurar arquivos estáticos (css, scripts, imgens)
 .use(express.static("public"))
 // rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
-.get("/give-classes", pageGiveclasses)
+.get("/give-classes", pageGiveClasses) 
+.post("/save-classes", saveClasses)
+// start do servidor
 .listen(5500)
 
- 
 
